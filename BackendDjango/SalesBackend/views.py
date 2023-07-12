5a204117eef8907194af1e52259d5cb43692e20c
khasamoh <khasamoh.12@gmail.com>
2023-07-13 02:11:16 +0300

    login

--- BackendDjango/SalesBackend/views.py
+++ BackendDjango/SalesBackend/views.py
@@ -1,9 +1,27 @@
+from django.contrib.auth import authenticate, login
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.views import APIView
+from rest_framework import generics
 from rest_framework.response import Response
 from SalesBackend.models import User,Product,Customer,Sales
 from .serializers import UserSerializer, ProductSerializer, SalesSerializer, CustomerSerializer
+
+
+#Login
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return JsonResponse({'message': 'Login successful'})
+        else:
+            return JsonResponse({'error': 'Invalid credentials'}, status=400)
+    else:
+        return JsonResponse({'error': 'Invalid request method'}, status=400)
+
 
 # Start API for User
 @api_view(['GET'])
@@ -31,6 +49,10 @@ def delete_user(request, user_id):
     user = User.objects.get(user_id=user_id)
     user.delete()
     return Response(status=204)
+
+# class UserSerializers(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = User.objects.all()
+#     serializer_class = UserSerializer
 
 # End API for User
 
